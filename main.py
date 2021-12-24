1cf353bde01a008cf9b9517cb43396e151d4a58b
Artem <gaposhaa1987@gmail.com>
2021-12-24 19:38:33 +0300

    remove first progect

---
+++ main.py
@@ -0,0 +1,11 @@
+class Information:
+    def __init__(self, name, secname, surname):
+        self.name = name
+        self.secname = secname
+        self. surname = surname
+    def type(self, equipment, infotmation):
+        self.equipment = equipment
+        self.information = infotmation
+        if equipment == "phone":
+            infotmation = [
+
