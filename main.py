a06c7ecfab96831a237f6fa4158ba21d1a5c0dee
Artem <gaposhaa1987@gmail.com>
2022-01-03 23:28:19 +0300

    Edit commit

--- main.py
+++ main.py
@@ -15,99 +15,60 @@ class Receipt:
         self.equipment = equipment
 
 
-print("""Приветствуем Вас в нашем сервисе по ремонту техники!
-Пожалуйста, введите следующие данные:
-            - "Имя"
-            - "Очество"
-            - "Фамилия"
-            - "Тип техники(Телефон, Телевизор, Ноутбук)""")
-
-user_data = Receipt(input("Имя - "), input("Очество - "), input("Фамилия - "), input("Тип техники - "))
-date_of_receipt = datetime.date.today()
-numbers_days = 1, 2, 3, 4, 5
-deadline = date_of_receipt + datetime.timedelta(random.choice(numbers_days))
-full_description = f"""№ квитанции: "{random.randint(1, 1000)}"
-Ф.И.О. клиента: "{user_data.surname}  {user_data.name} {user_data.father_name}"
-Дата принятия в ремонт: {date_of_receipt}
-Дата выдачи после ремонта: {deadline}
-Статус: "{execution_of_works("Техника принята в ремонт")}"
-Техническая информация/информация о поломке:"""
-
-
 class Technic(ABC):
     @abstractmethod
-    def enter_data(self):
+    def receiving_data(self):
         pass
 
 
 class Phone(Technic):
-    def enter_data(self):
-        try:
-            model = input("Модель - ")
-            if model.isnumeric():
-                raise ValueError
-            operation_system = input("Оперционная система - ")
-            if operation_system.isnumeric():
-                raise ValueError
-            type_of_breakdown = input("Тип поломки - ")
-            if type_of_breakdown.isnumeric():
-                raise ValueError
-        except ValueError:
-            print("Введена не корректная информация. Пожалуйста, исправьте.")
-        else:
-            print(f"{full_description} {user_data.equipment} - {model=}, {operation_system=}, {type_of_breakdown=}")
-            return model, operation_system, type_of_breakdown
+    def receiving_data(self):
+        model = input("Модель - ")
+        operation_system = input("Оперционная система - ")
+        type_of_breakdown = input("Тип поломки - ")
+        print(f"{full_description} {user_data.equipment} - {model=}, {operation_system=}, {type_of_breakdown=}")
 
 
 class TV(Technic):
-    def enter_data(self):
+    def receiving_data(self):
         while True:
+            model = input("Модель - ")
             try:
-                model = input("Модель - ")
-                if model.isnumeric():
-                    raise ValueError
                 diagonal = int(input("Диагональ экрана - "))
-                type_of_breakdown = input("Тип поломки - ")
-                if type_of_breakdown.isnumeric():
-                    raise ValueError
             except ValueError:
                 print("Введена не корректная информация. Пожалуйста, исправьте.")
             else:
+                type_of_breakdown = input("Тип поломки - ")
                 print(f"{full_description} {user_data.equipment} - {model=}, {diagonal=}, {type_of_breakdown=}")
-                return model, diagonal, type_of_breakdown
+                break
 
 
 class Laptop(Technic):
-    def enter_data(self):
+    def receiving_data(self):
         while True:
+            model = input("Модель - ")
+            operation_system = input("Операционная система - ")
             try:
-                model = input("Модель - ")
-                if model.isnumeric():
-                    raise ValueError
-                operation_system = input("Операционная система - ")
-                if operation_system.isnumeric():
-                    raise ValueError
                 year_of_release = int(input("Год выпуска - "))
-                type_of_breakdown = input("Тип поломки - ")
-                if type_of_breakdown.isnumeric():
-                    raise ValueError
             except ValueError:
                 print("Введена не корректная информация. Пожалуйста, исправьте.")
             else:
-                print(f"{full_description} {user_data.equipment} - {model=}, {year_of_release=}, {type_of_breakdown=}")
-                return model, year_of_release, operation_system, type_of_breakdown
+                type_of_breakdown = input("Тип поломки - ")
+                print(f"""{full_description} {user_data.equipment} - {model=}, {year_of_release=}, {operation_system=}
+{type_of_breakdown=}""")
+                break
 
 
-def output_data(type_of_technic):
+def send_data(type_of_technic):
     if user_data.equipment == "Телефон":
-        type_of_technic = Phone()
-        full_description, type_of_technic.enter_data()
+        current_phone = Phone()
+        current_phone.receiving_data()
     elif user_data.equipment == "Телевизор":
-        type_of_technic = TV()
-        full_description, type_of_technic.enter_data()
+        current_tv = TV()
+        current_tv.receiving_data()
     elif user_data.equipment == "Ноутбук":
-        type_of_technic = Laptop()
-        full_description, type_of_technic.enter_data()
+        current_laptop = Laptop()
+        current_laptop.receiving_data()
     else:
         print(f"""К сожалению, {user_data.name} {user_data.father_name}, мы не обслуживаем данный вид техники,      
 либо вы ввели не корректные данные.
@@ -116,8 +77,25 @@ def output_data(type_of_technic):
 2 - "Телевизор"
 3 - "Ноутбук" """)
         user_data.equipment = input("Тип техники(Телефон, Телевизор, Ноутбук) = ")
-        output_data(user_data.equipment)
-        return type_of_technic
+        send_data(user_data.equipment)
+    return type_of_technic
 
 
-output_data(user_data.equipment)
+if __name__ == "__main__":
+    print("""Приветствуем Вас в нашем сервисе по ремонту техники!
+    Пожалуйста, введите следующие данные:
+                - "Имя"
+                - "Очество"
+                - "Фамилия"
+                - "Тип техники(Телефон, Телевизор, Ноутбук)""")
+
+    user_data = Receipt(input("Имя - "), input("Очество - "), input("Фамилия - "), input("Тип техники - "))
+    date_of_receipt = datetime.date.today()
+    deadline = date_of_receipt + datetime.timedelta(random.choice(range(1, 5)))
+    full_description = f"""№ квитанции: "{random.randint(1, 1000)}"
+    Ф.И.О. клиента: "{user_data.surname}  {user_data.name} {user_data.father_name}"
+    Дата принятия в ремонт: {date_of_receipt}
+    Дата выдачи после ремонта: {deadline}
+    Статус: "{execution_of_works("Техника принята в ремонт")}"
+    Техническая информация/информация о поломке:"""
+    send_data(user_data.equipment)
